b2d1ba929156a8867cf248f9002a2ef219b93373
Mykytenko Petro <mikitenkot8@gmail.com>
2026-04-26 15:26:58 +0300

    WIP auth backend

--- WorldITSocialNetwork/user_app/forms.py
+++ WorldITSocialNetwork/user_app/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth import authenticate, get_user_model
+
+User = get_user_model()
 
 class RegisterForm(forms.Form):
     email = forms.EmailField(
@@ -13,7 +16,23 @@ class RegisterForm(forms.Form):
         widget=forms.PasswordInput(attrs= {'placeholder': 'Повтори пароль'}),
         label='Підтвердження паролю'
     )
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Пользователь с таким email вже є')
+        return email
 
+    def clean(self):
+        cleaned_data = super().clean()
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+        
+        if password and confirm_password and password == confirm_password:
+            return cleaned_data
+        
+        raise forms.ValidationError('Паролі не співпадають')
+    
 class LoginForm(forms.Form):
     email = forms.EmailField(
         label='Електронна пошта', 
@@ -22,16 +41,42 @@ class LoginForm(forms.Form):
     password = forms.CharField(
         widget=forms.PasswordInput(attrs= {'placeholder': 'Введи пароль'})
     )
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+        if not email:
+            return cleaned_data
+        if email and password:
+            user = authenticate(email=email, password=password)
+            if not user:
+                raise forms.ValidationError('Невірний пароль')
+        return cleaned_data
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email= email).exists():
+            raise forms.ValidationError('Користувач з таким email вже є')
+    
 
 class ConfirmEmailForm(forms.Form):
-    code = forms.CharField(
-        max_length=6,
-        min_length=6,
-        widget=forms.TextInput(attrs={
-            'class': 'otp-input',
-            'placeholder': '······',
-            'type': 'text',
-            'autocomplete': 'one-time-code', # Помогает автозаполнению из SMS/почты
-            'pattern': r'\d{6}', # Подсказка для браузера, что нужны только цифры
-        })
-    )
+    email1 = forms.CharField(max_length=1, min_length=1, required=True)
+    email2 = forms.CharField(max_length=1, min_length=1, required=True)
+    email3 = forms.CharField(max_length=1, min_length=1, required=True)
+    email4 = forms.CharField(max_length=1, min_length=1, required=True)
+    email5 = forms.CharField(max_length=1, min_length=1, required=True)
+    email6 = forms.CharField(max_length=1, min_length=1, required=True)
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        emailconfirm = (
+            cleaned_data.get("email1", "") +
+            cleaned_data.get("email2", "") +
+            cleaned_data.get("email3", "") +
+            cleaned_data.get("email4", "") +
+            cleaned_data.get("email5", "") +
+            cleaned_data.get("email6", "")
+        )
+        if len(emailconfirm) !=6:
+            raise forms.ValidationError("Код невірний")
+        return cleaned_data
--- WorldITSocialNetwork/user_app/views.py
+++ WorldITSocialNetwork/user_app/views.py
@@ -1,12 +1,18 @@
 from django.views.generic.base import TemplateView
-from typing import Any
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import redirect
+from django.contrib.auth import get_user_model
+
 from .forms import RegisterForm, LoginForm, ConfirmEmailForm
+
+
+User = get_user_model()
+
 
 class AuthTemplateView(TemplateView):
     template_name = 'user_app/auth.html'
 
-    def get_context_data(self, **kwargs) -> dict[str, Any]:
+    def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
         context['form_register'] = RegisterForm()
         context['form_login'] = LoginForm()
@@ -14,3 +20,17 @@ class AuthTemplateView(TemplateView):
         return context
 
 
+class RegisterView(View):
+    def post(self, request, *args, **kwargs):
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            User.objects.create_user(
+                username=form.cleaned_data['email'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+            )
+
+            return redirect('auth')
+
+        return redirect('auth')
