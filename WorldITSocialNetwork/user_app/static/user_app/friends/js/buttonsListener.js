2f77b1de8a04f8f018751314e93dcacbbe760b4a
Mykytenko Petro <mikitenkot8@gmail.com>
2026-05-22 17:17:45 +0300

    Hot fix auth

--- WorldITSocialNetwork/user_app/static/user_app/friends/js/buttonsListener.js
+++ WorldITSocialNetwork/user_app/static/user_app/friends/js/buttonsListener.js
@@ -27,7 +27,7 @@ allFriendsButton.forEach((element) => {
 // actions
 document.addEventListener("click", async (event) => {
     const element = event.target
-
+    
     console.log(element)
 
     if (!element.matches("[data-action]")) {
