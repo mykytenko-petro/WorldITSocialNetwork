26dbc401703f8d6bb9f88b3ebbf2427155bb5e49
andrewskulskuia <andrewskulskyia@gmail.com>
2026-05-29 18:33:18 +0300

    WIP

--- WorldITSocialNetwork/chat_app/models.py
+++ WorldITSocialNetwork/chat_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+user = get_user_model()
+
+class Chat(models.Model):
+    # 
+    users = models.ManyToManyField(to= user, related_name= "chats")
+    admin = models.ForeignKey(to= user, blank= True, null= True, on_delete= models.CASCADE)
+    
+    name = models.CharField(max_length= 30, blank= True, null= True)
+    is_group = models.BooleanField(default= False)
+    avatar = models.ImageField(upload_to= "chat_app/chat_avatars/", blank= True, null= True)
+
+    def __str__(self):
+        return self.name or f"Chat: {self.id}"
+    
+class Message(models.Model):
+    sender = models.ForeignKey(to=user, on_delete=models.CASCADE)
+    readers = models.ForeignKey(to=user, on_delete=models.CASCADE)
+    chat = models.ForeignKey(to=Chat, on_delete=models.CASCADE)
+
+    text = models.TextField()
+    created_at = models.DateTimeField()
+
+class MessageImage(models.Model):
+    message = models.ForeignKey(to=Message, on_delete=models.CASCADE)
+    image = models.ImageField()
--- WorldITSocialNetwork/chat_app/views.py
+++ WorldITSocialNetwork/chat_app/views.py
@@ -1,5 +1,27 @@
-from django.views.generic.base import TemplateView
+from django.shortcuts import render
+from django.views.generic import TemplateView, FormView, View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest, JsonResponse
 
+
+from .forms import MessageForm
+from .models import Chat
+from user_app.utils import get_all_friends
 # Create your views here.
-class ChatView(TemplateView):
-    template_name = 'chat_app/chat.html'
+
+User = get_user_model()
+
+class ChatView(
+        LoginRequiredMixin,
+        # FormView, 
+        TemplateView
+    ):
+    template_name = "chat_app/chat.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["friends"] = get_all_friends(self.request.user)
+        context["personal_chats"] = Chat.objects.filter(users= self.request.user, is_group= False).order_by("id")
+        
+        return context
