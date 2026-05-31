a18c39904fe918d4a0f8dac2ff611897f944c55c
Mykytenko Petro <mikitenkot8@gmail.com>
2026-05-31 23:08:35 +0300

    added pythonanywhere postgersql connection functionality and refactored profile model

--- WorldITSocialNetwork/WorldITSocialNetwork/settings.py
+++ WorldITSocialNetwork/WorldITSocialNetwork/settings.py
@@ -8,114 +8,141 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(dotenv_path=BASE_DIR / ".env")
 
-SECRET_KEY = os.getenv("SECRET_KEY") if os.getenv("SECRET_KEY") else 'django-insecure-(l^y*xm_umz7lfx5j@k3ttgm*j5(rd94zl&$h+-zkrr9zu_wlo'
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "django-insecure-(l^y*xm_umz7lfx5j@k3ttgm*j5(rd94zl&$h+-zkrr9zu_wlo"
+)
 
 DEBUG = False if os.getenv("DEBUG") == "False" else True
 
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'daphne',
-    
+    # server
+    "daphne",
+
     # middleware
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
-    'channels',
-    
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "channels",
+
     # project apps
-    'chat_app',
-    'home_app',
-    'post_app',
-    'profile_app',
-    'user_app',
+    "chat_app",
+    "home_app",
+    "post_app",
+    "profile_app",
+    "user_app",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'WorldITSocialNetwork.urls'
+ROOT_URLCONF = "WorldITSocialNetwork.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates',
-            BASE_DIR / 'chat_app' / 'templates',
-            BASE_DIR / 'home_app' / 'templates',
-            BASE_DIR / 'post_app' / 'templates',
-            BASE_DIR / 'profile_app' / 'templates',
-            BASE_DIR / 'user_app' / 'templates',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "templates",
+            BASE_DIR / "chat_app" / "templates",
+            BASE_DIR / "home_app" / "templates",
+            BASE_DIR / "post_app" / "templates",
+            BASE_DIR / "profile_app" / "templates",
+            BASE_DIR / "user_app" / "templates",
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-
-
-
+# websockets
 CHANNEL_LAYERS = {
-    
-    'default': {
-       
-        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
     }
 }
-
-
 
 # auth
-LOGIN_URL = 'user_app.auth'
-AUTH_USER_MODEL = 'user_app.User'
+LOGIN_URL = "user_app.auth"
+AUTH_USER_MODEL = "user_app.User"
 
-# WSGI_APPLICATION = 'WorldITSocialNetwork.wsgi.application'
-ASGI_APPLICATION = 'WorldITSocialNetwork.asgi.application'
+# asgi
+ASGI_APPLICATION = "WorldITSocialNetwork.asgi.application"
+
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if not os.getenv("REMOTE_DB_NAME"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    from sshtunnel import SSHTunnelForwarder
+
+    if os.getenv("SSH_LOGIN"):
+        tunnel = SSHTunnelForwarder(
+            ('ssh.pythonanywhere.com', 22),
+            ssh_username=os.getenv("SSH_LOGIN"),
+            ssh_password=os.getenv("SSH_PASSWORD"),
+            remote_bind_address=(os.getenv("REMOTE_DB_HOST"), int(os.getenv("REMOTE_DB_PORT"))), # type: ignore
+        )
+
+        tunnel.start()
+
+        port = str(tunnel.local_bind_port)
+    else:
+        port = os.getenv("REMOTE_DB_PORT")
+
+    DATABASES = {
+        "default": {
+            'ENGINE': os.getenv("REMOTE_DB_ENGINE"),
+            'NAME': os.getenv("REMOTE_DB_NAME"),
+            'USER': os.getenv("REMOTE_DB_USER"),
+            'PASSWORD': os.getenv("REMOTE_DB_PASSWORD"),
+            'HOST': '127.0.0.1', 
+            'PORT': port,
+            'CONN_MAX_AGE': 600
+        }
+    }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -123,24 +150,24 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [ 
-    BASE_DIR / 'static',
-    BASE_DIR / 'chat_app' / 'static',
-    BASE_DIR / 'home_app' / 'static',
-    BASE_DIR / 'post_app' / 'static',
-    BASE_DIR / 'profile_app' / 'static',
-    BASE_DIR / 'user_app' / 'static',
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "chat_app" / "static",
+    BASE_DIR / "home_app" / "static",
+    BASE_DIR / "post_app" / "static",
+    BASE_DIR / "profile_app" / "static",
+    BASE_DIR / "user_app" / "static",
 ]
 
 # SMTP
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 # Media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- WorldITSocialNetwork/post_app/models.py
+++ WorldITSocialNetwork/post_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from PIL import Image
+from django.core.exceptions import SuspiciousFileOperation
 
 
 class Post(models.Model):
@@ -42,16 +43,18 @@ class PostImage(models.Model):
     def __str__(self) -> str:
         return self.original_image.name
     
-    # known issue: if it will be moved to cloud storage it would break
-    # solution is make width and height fields later
     @property
     def orientation(self):
-        with Image.open(self.compressed_image.path) as img:
-            if img.width > img.height:
-                return "landscape"
-            
-            elif img.height >= img.width:
+        if not self.compressed_image:
+            return "unknown"
+        
+        try:
+            with Image.open(self.compressed_image) as img:
+                if img.width > img.height:
+                    return "landscape"
                 return "portrait"
+        except (FileNotFoundError, ValueError, SuspiciousFileOperation):
+            return "unknown"
 
 
 class PostView(models.Model):
