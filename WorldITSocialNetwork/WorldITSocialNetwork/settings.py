4c48be2d99b13612fc98d111080074e8601836f8
Mykytenko Petro <mikitenkot8@gmail.com>
2026-05-31 23:59:33 +0300

    Add debug toolbar configuration to settings and urls

--- WorldITSocialNetwork/WorldITSocialNetwork/settings.py
+++ WorldITSocialNetwork/WorldITSocialNetwork/settings.py
@@ -15,6 +15,10 @@ SECRET_KEY = os.getenv(
 
 DEBUG = False if os.getenv("DEBUG") == "False" else True
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
@@ -29,6 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "channels",
+    "debug_toolbar",
 
     # project apps
     "chat_app",
@@ -46,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "WorldITSocialNetwork.urls"
