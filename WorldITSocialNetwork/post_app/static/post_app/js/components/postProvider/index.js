adbd364fb13b4eac0650ff05ecfcfbecc7e9e2e8
andrewskulskuia <andrewskulskyia@gmail.com>
2026-06-02 22:37:19 +0300

    end of working on friend page

--- WorldITSocialNetwork/post_app/static/post_app/js/components/postProvider/index.js
+++ WorldITSocialNetwork/post_app/static/post_app/js/components/postProvider/index.js
@@ -1,17 +1,19 @@
 import { PaginationProvider } from "/static/js/utils/paginationProvider.js"
 
-// dom
 const postProvider = document.getElementById("postProvider")
-
-// logic
-let mode = "own_posts"
 const baseUrl = postProvider.dataset.url
 const userId = postProvider.dataset.userId
-const url = `/user/${userId}?userId=${userId}/`
+
+let queryParams
+if (userId) {
+    queryParams = `&mode=own_posts&user_id=${userId}`
+} else {
+    queryParams = `&mode=recommendations`
+}
 
 new PaginationProvider(
     baseUrl,
     postProvider,
     null,
-    `&mode=${mode}&user_id=${userId}`
+    queryParams
 )
