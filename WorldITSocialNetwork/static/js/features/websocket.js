7ac7f426b728da1576a34c391769cd6d6a30adc7
Mykytenko Petro <mikitenkot8@gmail.com>
2026-06-21 16:13:24 +0300

    fix: Correct CSS formatting and adjust main content height for better layout consistency

--- WorldITSocialNetwork/static/js/features/websocket.js
+++ WorldITSocialNetwork/static/js/features/websocket.js
@@ -1,5 +1,6 @@
 // const WebsocketBackendURL = "http://localhost:2232"
-const WebsocketBackendURL = "https://yu-unabiding-vampishly.ngrok-free.dev"
+// const WebsocketBackendURL = "https://yu-unabiding-vampishly.ngrok-free.dev"
+const WebsocketBackendURL = "http://192.168.0.115:8000"
 
 async function getToken() {
     const res = await fetch("/socket_token/", {
